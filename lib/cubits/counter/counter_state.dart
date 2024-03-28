b467551a2641d122c4d91c1158aafb54685ac3a3
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-27 20:42:54 -0600

    refactor: Code own implementation of state

--- lib/cubits/counter/counter_state.dart
+++ lib/cubits/counter/counter_state.dart
@@ -1,10 +1,36 @@
-part of 'counter_cubit.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
 
-sealed class CounterState extends Equatable {
-  const CounterState();
+class CounterState extends Equatable {
+  final int counter;
+  const CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return const CounterState(counter: 0);
+  }
+  
+  @override
+  List<Object> get props => [counter];
 
   @override
-  List<Object> get props => [];
+  String toString() => 'CounterState(counter $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
 }
 
-final class CounterInitial extends CounterState {}
+// sealed class CounterState extends Equatable {
+//   const CounterState();
+
+//   @override
+//   List<Object> get props => [];
+// }
+
+// final class CounterInitial extends CounterState {}
