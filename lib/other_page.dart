a3d0b6879ec78dfa4a746ba46181e852521839f2
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-01 17:38:21 -0600

    chore: Wrap widget inside blocprovider

--- lib/other_page.dart
+++ lib/other_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc_app/blocs/counter/counter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OtherPage extends StatelessWidget {
-  const OtherPage({super.key,});
-  
+  const OtherPage({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Other'),
-      ),
-      body: Center(
-        child: Column(
+    return BlocProvider<CounterBloc>(
+      create: (context) => CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Other'),
+        ),
+        body: Center(
+            child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
@@ -23,29 +28,25 @@ class OtherPage extends StatelessWidget {
               style: Theme.of(context).textTheme.bodyMedium,
             ),
           ],
-        )
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              
-            },
-            tooltip: 'increment',
-            heroTag: 'increment',    
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: () {
-              
-            },
-            tooltip: 'decrement',
-            heroTag: 'decrement',    
-            child: const Icon(Icons.remove),
-          ),
-        ],
+        )),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'increment',
+              heroTag: 'increment',
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'decrement',
+              heroTag: 'decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
       ),
     );
   }
