b23e72916466ccf371bc6526049e31643e55a756
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-01 17:25:52 -0600

    chore: Add handlers to events

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -5,9 +7,18 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterBloc() : super(CounterState.initial()) {
+    on<IncrementCounterEvent>((event, emit) {
+      emit(state.copyWith(counter: state.counter + 1));
     });
+
+    on<DecrementCounterEvent>(_decrementCounter);
   }
+
+  void _decrementCounter(
+    DecrementCounterEvent event,
+    Emitter<CounterState> emit
+    ) {
+      emit(state.copyWith(counter: state.counter - 1));
+    }
 }
