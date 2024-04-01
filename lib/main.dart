339fac02b9143fc752ee13b8426e5f9288f203f0
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-01 12:56:08 -0600

    chore: wrap blocbuilder inside a bloclistener to be able to perform one time actions like displaying a dialog

--- lib/main.dart
+++ lib/main.dart
@@ -88,58 +88,55 @@ class _MyHomePageState extends State<MyHomePage> {
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body: BlocBuilder<CounterCubit, CounterState>(
-          builder: (context, state) {
-            if (state.counter == 3) {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    content: Text('counter is ${state.counter}'),
-                  );
-                } 
-              );
-            } else if (state.counter == -1) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const OtherPage();
-                  }
-                )
-              );
-            }
-
-            return Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                //
-                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-                // action in the IDE, or press "p" in the console), to see the
-                // wireframe for each widget.
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Counter',
-                  ),
-                  Text(
-                    '${state.counter}',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            );
+        body: BlocListener<CounterCubit, CounterState>(
+          listener: (context, state) {        
+              if (state.counter == 3) {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('counter is ${state.counter}'),
+                      );
+                    });
+              } else if (state.counter == -1) {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const OtherPage();
+                }));
+              }
           },
+          child: BlocBuilder<CounterCubit, CounterState>(
+            builder: (context, state) {
+              return Center(
+                // Center is a layout widget. It takes a single child and positions it
+                // in the middle of the parent.
+                child: Column(
+                  // Column is also a layout widget. It takes a list of children and
+                  // arranges them vertically. By default, it sizes itself to fit its
+                  // children horizontally, and tries to be as tall as its parent.
+                  //
+                  // Column has various properties to control how it sizes itself and
+                  // how it positions its children. Here we use mainAxisAlignment to
+                  // center the children vertically; the main axis here is the vertical
+                  // axis because Columns are vertical (the cross axis would be
+                  // horizontal).
+                  //
+                  // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                  // action in the IDE, or press "p" in the console), to see the
+                  // wireframe for each widget.
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Counter',
+                    ),
+                    Text(
+                      '${state.counter}',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
