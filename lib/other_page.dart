3c00f518927410b57ed865c54d0a3f8543c57b7c
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-01 16:36:40 -0600

    chore: Prepare other screen to apply bloc

--- lib/other_page.dart
+++ lib/other_page.dart
@@ -9,11 +9,37 @@ class OtherPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Other'),
       ),
-      body: const Center(
-        child: Text(
-          'Other',
-          style: TextStyle(fontSize: 52)
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Other counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              '0',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: const Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: onPressed,
+            tooltip: 'increment',
+            heroTag: 'increment',    
+            child: Icon(Icons.add),
           ),
+          FloatingActionButton(
+            onPressed: onPressed,
+            tooltip: 'decrement',
+            heroTag: 'decrement',    
+            child: Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
