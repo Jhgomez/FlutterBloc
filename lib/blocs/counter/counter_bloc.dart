c5e5028a0255765252fb1cb74583afe812d403eb
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-04 16:17:50 -0600

    refactor: Register bloc's events under the same handler and specify sequential transformations

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
@@ -6,17 +7,37 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState.initial()) {
-    on<IncrementCounterEvent>((event, emit) {
-      emit(state.copyWith(counter: state.counter + 1));
-    });
+    // on<IncrementCounterEvent>((event, emit) {
+    //   emit(state.copyWith(counter: state.counter + 1));
+    // });
+    
 
-    on<DecrementCounterEvent>(_decrementCounter);
+    // Even thought these buckets run sequentially since I specified it
+    // their stack runs asynchronously because they are separated but in the
+    // bellow code they run sequentially because they are in the same bucker 
+    // on<IncrementCounterEvent>(_incrementCounter);
+    // on<DecrementCounterEvent>(_decrementCounter);
+    on<CounterEvent>( ((event, emit) async {
+          switch(event) {
+            case IncrementCounterEvent() : await _incrementCounter(event, emit);
+            case DecrementCounterEvent() : await _decrementCounter(event, emit);
+          }
+        }
+      ),
+      transformer: sequential()
+    );
   }
 
-  void _decrementCounter(
+  Future<void> _decrementCounter(
     DecrementCounterEvent event,
     Emitter<CounterState> emit
-    ) {
+    ) async {
+      await Future.delayed(const Duration(seconds: 2));
+      emit(state.copyWith(counter: state.counter - 1));
+    }
+
+    Future<void> _incrementCounter(event,emit) async {
+      await Future.delayed(const Duration(seconds: 4));
       emit(state.copyWith(counter: state.counter - 1));
     }
 }
