73b650a7e5e460c22e16670746c3372ec5e51969
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-04 11:35:56 -0600

    chore: Add print to bloc observer functions

--- lib/observer/app_bloc_observer.dart
+++ lib/observer/app_bloc_observer.dart
@@ -4,24 +4,24 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    // TODO: implement onEvent
+    print('onEvent -- ${bloc.runtimeType}: $event');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
     super.onError(bloc, error, stackTrace);
+    print('onError -- ${bloc.runtimeType}: $error, $stackTrace');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    // TODO: implement onChange
+    print('onChange -- ${bloc.runtimeType}: $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    // TODO: implement onChange
+    print('onTransmition -- ${bloc.runtimeType}: $transition');
   }
 }
