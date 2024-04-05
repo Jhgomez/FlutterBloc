95fd0a947de8ffa7b2e063151b0d27c6b9bb06bf
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-05 00:20:28 -0600

    chore: wrap materialapp inside a multiblocprovider and at the same time inside a builder to have access to the blocs that where injected

--- lib/main.dart
+++ lib/main.dart
@@ -17,31 +17,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Counter Cubit',
-        theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // TRY THIS: Try running your application with "flutter run". You'll see
-            // the application has a purple toolbar. Then, without quitting the app,
-            // try changing the seedColor in the colorScheme below to Colors.green
-            // and then invoke "hot reload" (save your changes or press the "hot
-            // reload" button in a Flutter-supported IDE, or press "r" if you used
-            // the command line to start the app).
-            //
-            // Notice that the counter didn't reset back to zero; the application
-            // state is not lost during the reload. To reset the state, use hot
-            // restart instead.
-            //
-            // This works for code too, not just values: Most code changes can be
-            // tested with just a hot reload.
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-            primarySwatch: Colors.blue),
-        debugShowCheckedModeBanner: false,
-        home: const MyHomePage(title: 'Flutter Bloc Home Page'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CounterCubit>(create: (context) => CounterCubit()),
+        BlocProvider<ThemeBloc>(create: (context) => ThemeBloc())
+      ],
+      child: Builder(
+        builder: (contextB) {
+          return MaterialApp(
+            title: 'Counter Cubit',
+            theme: contextB.watch<ThemeBloc>().state.colorTheme == ColorTheme.DARK ? ThemeData.dark() : ThemeData.light(),
+            // ThemeData(
+            //     // This is the theme of your application.
+            //     //
+            //     // TRY THIS: Try running your application with "flutter run". You'll see
+            //     // the application has a purple toolbar. Then, without quitting the app,
+            //     // try changing the seedColor in the colorScheme below to Colors.green
+            //     // and then invoke "hot reload" (save your changes or press the "hot
+            //     // reload" button in a Flutter-supported IDE, or press "r" if you used
+            //     // the command line to start the app).
+            //     //
+            //     // Notice that the counter didn't reset back to zero; the application
+            //     // state is not lost during the reload. To reset the state, use hot
+            //     // restart instead.
+            //     //
+            //     // This works for code too, not just values: Most code changes can be
+            //     // tested with just a hot reload.
+            //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            //     useMaterial3: true,
+            //     primarySwatch: Colors.blue),
+            debugShowCheckedModeBanner: false,
+            home: const MyHomePage(title: 'Flutter Bloc Home Page'),
+          );
+        }
       ),
     );
   }
@@ -107,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   return MultiBlocProvider(
                     providers: [
                       BlocProvider<CounterBloc>(create: (contextRoute) => CounterBloc()),
-                      BlocProvider<ThemeBloc>(create: (contextRoute) => ThemeBloc())
+                      // BlocProvider<ThemeBloc>(create: (contextRoute) => context.read<ThemeBloc>())
                     ],
                     child: const OtherPage()
                     );
