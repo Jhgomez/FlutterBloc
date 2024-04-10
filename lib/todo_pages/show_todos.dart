9c9b73472b1c51cd0dfd96bb570fd2d7da8239d8
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-10 12:49:29 -0600

    chore: update todo list and filtered list when toggling todo checkbox

--- lib/todo_pages/show_todos.dart
+++ lib/todo_pages/show_todos.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_app/cubits/todo_cubits/filtered_todos/filtered_todos_cubit.dart';
+import 'package:flutter_bloc_app/cubits/todo_cubits/todo_cubits.dart';
+import 'package:flutter_bloc_app/todo_models/todo.dart';
 
 
 class TodoList extends StatelessWidget {
@@ -17,12 +18,9 @@ class TodoList extends StatelessWidget {
             key: ValueKey(todos[index].id),
             background: showBackground(0),
             secondaryBackground: showBackground(1),
-            child: Text(
-              todos[index].desc,
-              style: const TextStyle(fontSize: 20)
-            ),
-            onDismissed: (todo) {
-
+            child: TodoItem(todo: todos[index]),
+            onDismissed: (_) {
+              // context.read<TodoListCubit>().removeTodo(todos[index].id);
             },
           );
         }
@@ -38,7 +36,7 @@ class TodoList extends StatelessWidget {
   Widget showBackground(int direction) {
     return Container(
       margin: const EdgeInsets.all(4),
-      padding: const EdgeInsets.symmetric(10),
+      padding: const EdgeInsets.symmetric(),
       color: Colors.red,
       alignment: direction == 0 ? Alignment.centerLeft : Alignment.centerRight,
       child: const Icon(
@@ -46,6 +44,34 @@ class TodoList extends StatelessWidget {
         size: 30,
         color: Colors.white,
       ),
-    )
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+  const TodoItem({super.key, required this.todo});
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile.adaptive(
+      value: widget.todo.completed,
+      onChanged: (_) {
+        context.read<TodoListCubit>().toggleTodo(widget.todo.id);
+        context.read<FilteredTodosCubit>().updateFilteredTodos(
+          context.read<TodoListCubit>().state.todos, 
+          context.read<TodoFilterCubit>().state.filter, 
+          context.read<TodoSearchCubit>().state.searchTerm
+        );
+      },
+      title: Text(widget.todo.desc),
+      subtitle: const Text('Subtitle'),
+      activeColor: Colors.blue,
+    );
   }
 }
