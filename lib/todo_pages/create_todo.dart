4eb9610309dd60d4c17e82430fcc16848945a11c
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-10 00:19:03 -0600

    chore: update active count when submitting new todo

---
+++ lib/todo_pages/create_todo.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/cubits/todo_cubits/filtered_todos/filtered_todos_cubit.dart';
+import 'package:flutter_bloc_app/cubits/todo_cubits/todo_cubits.dart';
+import 'package:flutter_bloc_app/todo_models/todo.dart';
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({super.key});
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final TextEditingController newTodoController = TextEditingController();
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: InputDecoration(labelText: 'What to do?'),
+      onChanged: (term) {
+        // if (term.isEmpty) return;
+        // List<Todo> todos = context.read<TodoListCubit>().state.todos;
+        // Filter filter = context.read<TodoFilterCubit>().state.filter; 
+        // context.read<FilteredTodosCubit>().updateFilteredTodos(todos, filter, term);
+        },
+      onSubmitted: (desc) {
+        if (desc.isEmpty) return;
+        context.read<TodoListCubit>().addTodo(desc);
+        context.read<ActiveCountCubit>().updateActiveCount(context.read<TodoListCubit>().state.todos);
+        newTodoController.clear();
+        } ,
+    );
+  }
+}
