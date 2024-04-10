42d718d8812bec72abd93cfdbd9f9cf07c6f4e8a
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-09 22:54:56 -0600

    chore: inyect cubits

--- lib/todo_pages/todo_home.dart
+++ lib/todo_pages/todo_home.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/cubits/todo_cubits/todo_cubits.dart';
 
 class TodoHome extends StatelessWidget {
   static const routeName = '/todoHome';
@@ -8,9 +9,27 @@ class TodoHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: const Text('Todos'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(create: (context) => TodoFilterCubit()),
+        BlocProvider<TodoListCubit>(create: (context) => TodoListCubit()),
+        BlocProvider<TodoSearchCubit>(create: (context) => TodoSearchCubit()),
+        BlocProvider<ActiveCountCubit>(create: (context) {
+            int count = context.read<TodoListCubit>().state.todos.where((todo) => !todo.completed).toList().length;
+            return ActiveCountCubit(count);
+          }
+        ),
+        BlocProvider<FilteredTodosCubit>(create: (context) => FilteredTodosCubit(context.read<TodoListCubit>().state.todos)
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Center(
+              child: const Text('Todos'),
+            ),
+          );
+        }
       ),
     );
   }
