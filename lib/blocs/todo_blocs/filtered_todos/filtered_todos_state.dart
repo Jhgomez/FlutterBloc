1fbc53ca4d2e9340c5e5d8ec83c338fe7ca9d58c
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-11 11:13:40 -0600

    chore: Add filtered todos bloc

--- lib/blocs/todo_blocs/filtered_todos/filtered_todos_state.dart
+++ lib/blocs/todo_blocs/filtered_todos/filtered_todos_state.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-part of 'filtered_todos_cubit.dart';
+part of 'filtered_todos_bloc.dart';
 
 class FilteredTodosState extends Equatable {
   final List<Todo> todos;
--- lib/blocs/todo_blocs/todo_bloc.dart
+++ lib/blocs/todo_blocs/todo_bloc.dart
@@ -1,4 +1,4 @@
-export 'filtered_todos/filtered_todos_cubit.dart';
+export 'filtered_todos/filtered_todos_bloc.dart';
 export 'todo_list/todo_list_bloc.dart';
 export 'active_count/active_count_cubit.dart';
 export 'todo_filter/todo_filter_bloc.dart';
