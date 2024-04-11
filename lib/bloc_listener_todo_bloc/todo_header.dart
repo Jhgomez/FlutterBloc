0c57a4c0494fcfee29b43d1014e4064f8faa3117
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-11 13:14:21 -0600

    chore: trigger events in header

--- lib/bloc_listener_todo_bloc/todo_header.dart
+++ lib/bloc_listener_todo_bloc/todo_header.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_app/cubits/todo_cubits/todo_cubits.dart';
+import 'package:flutter_bloc_app/blocs/todo_blocs/todo_bloc.dart';
 
 class TodoHeader extends StatelessWidget {
   const TodoHeader({super.key});
@@ -14,12 +14,12 @@ class TodoHeader extends StatelessWidget {
           'TODO',
           style: TextStyle(fontSize: 40),
         ),
-        BlocListener<TodoListCubit, TodoListState>(
+        BlocListener<TodoListBloc, TodoListState>(
           listener: (context, state) {
-            context.read<ActiveCountCubit>().updateActiveCount(state.todos);
+            context.read<ActiveCountBloc>().add(UpdateCountEvent(todos: state.todos));
           },
           child: Text(
-            '${context.watch<ActiveCountCubit>().state.activeCount} items left',
+            '${context.watch<ActiveCountBloc>().state.activeCount} items left',
             style: const TextStyle(fontSize: 20, color: Colors.redAccent),
           ),
         )
