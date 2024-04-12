c53d4a97dc0be13593235a6cf96622ec326db63d
Juan Gomez <juan.e.g.f@outlook.com>
2024-04-11 21:26:51 -0600

    chore: Add transformer to search bloc

--- lib/blocs/todo_blocs/todo_search/todo_search_bloc.dart
+++ lib/blocs/todo_blocs/todo_search/todo_search_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'todo_search_state.dart';
 part 'todo_search_event.dart';
@@ -11,6 +12,11 @@ class TodoSearchBloc extends Bloc<TodoSearchEvent, TodoSearchState> {
         case UpdateTermEvent() :
           emit(state.copyWith(searchTerm: event.searchTerm));
       }
-    });
+    },
+    transformer: debounce(const Duration(milliseconds: 500)));
+  }
+
+  EventTransformer<UpdateTermEvent> debounce<UpdateTermEvent>(Duration duration) {
+    return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
   }
 }
